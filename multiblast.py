19ae90b8c14b5b1de998b083484183e560db2432
mark <markgodek@gmail.com>
2019-09-02 11:51:17 -0400

    fix: remove extra fasta_handle style: added index of results to output

--- multiblast.py
+++ multiblast.py
@@ -11,12 +11,14 @@ with open(output, 'a+') as output_handle:
     for filename in glob.glob(os.path.join(input, '*.fasta')):
         print(filename)
         with open(filename, 'rU') as fasta_handle:
-            with open(filename, 'rU') as fasta_handle:
-                result_handle = NCBIWWW.qblast('blastn', 'nt',
-                                               fasta_handle.read(), hitlist_size=5)
-                blast_results = SearchIO.parse(result_handle, 'blast-xml')
-                for result in blast_results:
-                    for hsp in result.hsps:
-                        output_handle.write(str(hsp) + '\n\n')
+            result_handle = NCBIWWW.qblast('blastn', 'nt',
+                                           fasta_handle.read(), hitlist_size=5)
+            blast_results = SearchIO.parse(result_handle, 'blast-xml')
+            for result in blast_results:
+                i = 1
+                for hsp in result.hsps:
+                    output_handle.write('Result #' + str(i) + '\n')
+                    output_handle.write(str(hsp) + '\n\n')
+                    i+=1
         fasta_handle.close()
 print('Done')
