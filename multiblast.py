124493f73e7935804b6e18298f87a268398126b3
mark <markgodek@gmail.com>
2019-09-02 11:47:25 -0400

    feature: added writing to output

--- multiblast.py
+++ multiblast.py
@@ -3,18 +3,20 @@ from Bio import SearchIO
 from Bio.Blast import NCBIWWW
 
 input = sys.argv[1]
+output = sys.argv[2]
 
 # /home/mark/PycharmProjects/MultiBlast/demoFASTAs
 
-for filename in glob.glob(os.path.join(input, '*.fasta')):
-    print(filename)
-    with open(filename, 'rU') as fasta_handle:
+with open(output, 'a+') as output_handle:
+    for filename in glob.glob(os.path.join(input, '*.fasta')):
+        print(filename)
         with open(filename, 'rU') as fasta_handle:
-            result_handle = NCBIWWW.qblast('blastn', 'nt',
-                                           fasta_handle.read(), hitlist_size=5)
-            blast_results = SearchIO.parse(result_handle, 'blast-xml')
-            for result in blast_results:
-                for hsp in result.hsps:
-                    print(str(hsp) + '\n\n')
+            with open(filename, 'rU') as fasta_handle:
+                result_handle = NCBIWWW.qblast('blastn', 'nt',
+                                               fasta_handle.read(), hitlist_size=5)
+                blast_results = SearchIO.parse(result_handle, 'blast-xml')
+                for result in blast_results:
+                    for hsp in result.hsps:
+                        output_handle.write(str(hsp) + '\n\n')
         fasta_handle.close()
 print('Done')
