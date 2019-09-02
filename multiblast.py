efcc3d238096cc28845593ca4143cda72fa4d1a5
mark <markgodek@gmail.com>
2019-09-02 07:24:26 -0400

    feature: added main code for BLAST search

--- multiblast.py
+++ multiblast.py
@@ -1,4 +1,6 @@
 import os, glob, sys
+from Bio import SearchIO
+from Bio.Blast import NCBIWWW
 
 input = sys.argv[1]
 
@@ -6,4 +8,13 @@ input = sys.argv[1]
 
 for filename in glob.glob(os.path.join(input, '*.fasta')):
     print(filename)
-
+    with open(filename, 'rU') as fasta_handle:
+        with open(filename, 'rU') as fasta_handle:
+            result_handle = NCBIWWW.qblast('blastn', 'nt',
+                                           fasta_handle.read(), hitlist_size=5)
+            blast_results = SearchIO.parse(result_handle, 'blast-xml')
+            for result in blast_results:
+                for hsp in result.hsps:
+                    print(str(hsp) + '\n\n')
+        fasta_handle.close()
+print('Done')
